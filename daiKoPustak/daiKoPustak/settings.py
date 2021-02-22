b56ba4979ab7e5828e51837b8c4947c9993ca25e
tsachin34 <jrsachinn99@gmail.com>
2021-02-22 11:05:33 +0545

    project setup

--- daiKoPustak/daiKoPustak/settings.py
+++ daiKoPustak/daiKoPustak/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'daiKoPustak.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'daikopustak',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
